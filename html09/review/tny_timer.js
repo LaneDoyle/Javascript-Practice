61013717550752bba7a303d6e86f788737260121
LaneDoyle <ldoyle@stcharlessd.org>
2020-02-21 13:38:47 -0600

    modified:   html09/review/tny_timer.js

--- html09/review/tny_timer.js
+++ html09/review/tny_timer.js
@@ -18,17 +18,17 @@ function showClock() {
     var localDate = new Date("May 19, 2018");
     var localTime =  new Date("9:31:27 AM");
     document.getElementById("currentTime").innerHTML = "<span>May 19, 2018</span><span>9:31:27 AM</span>";
-    j4Date = nextJuly4(thisDay);
+    var j4Date = nextJuly4(thisDay);
     j4Date.setHours("21:00:00");
-    var days = (cYear - jDate) / (1000 * 60 * 60 * 24);
+    var days = (thisDay.getFullYear() - j4Date) / (1000 * 60 * 60 * 24);
     var hrs = (days - Math.floor(days))* 24;
     var mins = (hrs - Math.floor(hrs)* 60);
     var secs = (mins - Math.floor(mins)* 60);
-    document.getElementById("dLeft").textcontent = Math.floor(days);
+    document.getElementById("dLeft").textContent = Math.floor(days);
     document.getElementById("hLeft").textContent = Math.floor(hrs);
     document.getElementById("mLeft").textContent = Math.floor(mins);
-    document.getElementById("sLeft").textContent = Math.floor(secs)
-;
+    document.getElementById("sLeft").textContent = Math.floor(secs);
+
 }
 
 function nextJuly4(currentDate) {
